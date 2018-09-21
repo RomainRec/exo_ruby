db0a8e3cc52871b27b8f847eb6c640f56b643fd9
RomainRec <romain.recher@gmail.com>
2018-09-21 07:31:20 -0400

    ajout de tous les fichiers exo dans le dossier

---
+++ exo_07_a.rb
@@ -0,0 +1,5 @@
+puts "Bonjour, c'est quoi ton blase ?"
+user_name = gets.chomp
+puts user_name
+
+# get.chomp invite l'utilisateur à rentrer une donée.
---
+++ exo_07_c.rb
@@ -0,0 +1,6 @@
+user_name = gets.chomp
+puts user_name
+
+# exo_07_a demande le nom de l'utilisateur et affiche le nom
+# exo_07_b demande le nom de l'utilisateur en ajoutant un < et affiche le nom
+# exo_07_b ne demande pas le nom de l'utilisateur et affiche la réponse directement
