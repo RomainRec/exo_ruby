175f7c9611708065830462d2a81d788c78d82d8f
RomainRec <romain.recher@gmail.com>
2018-09-21 08:51:22 -0400

    exo 11 ok3

--- exo_10.rb
+++ exo_10.rb
@@ -4,6 +4,6 @@ puts "Bonjour, quelle est ton année de naissance ?"
 print "> "
 user_birth = gets.chomp
 puts "En 2017 tu avais :"
-calcul = (2017 - user_birth.to_i)
+calcul = (year - user_birth.to_i)
 puts calcul
 puts "Incroyable non ?"
